d66ae9fb931cb7fa9e38624fc8f0ca52225d0abc
Alex <alexis.autres@gmail.com>
2025-06-07 18:26:55 +0200

    feat(interfaces): Add OlympicCountry and Participation

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,11 +1,7 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
-{
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
+export interface Participation {
+  id: number; // 1
+  year: number; // 2012
+  city: string; // "Londres"
+  medalsCount: number; // 28
+  athleteCount: number; // 372
 }
-*/
